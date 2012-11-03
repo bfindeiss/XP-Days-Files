1c180900145b61f5e20e70975f626573647899ca
Bernhard Findeiss <bernhard.findeiss@googlemail.com>
2012-11-03 17:42:58 +0100

    Initial Commit

---
+++ XP Days 2012 - 2.mm
@@ -0,0 +1,190 @@
+<map version="1.0.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1351939154272" ID="ID_779446371" MODIFIED="1351960345638" TEXT="XP Days 2012">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="font-weight: normal; letter-spacing: normal; text-indent: 0px; line-height: normal; white-space: normal; font-variant: normal; color: rgb(0, 0, 0); font-family: arial, helvetica, sans-serif; font-size: 11px; margin-bottom: 0; margin-right: 1px; background-color: rgb(255, 255, 255); margin-top: 0; text-transform: none; font-style: normal; margin-left: 1px; word-spacing: 0px; text-align: justify" charset="utf-8" class="paper_abstract">
+      Testgetriebene Entwicklung ist aus meiner Erfahrung eine der erfolgreichsten Vorgehensweisen in der Softwareentwicklung.
+    </p>
+    <p style="font-weight: normal; letter-spacing: normal; text-indent: 0px; line-height: normal; white-space: normal; font-variant: normal; color: rgb(0, 0, 0); font-family: arial, helvetica, sans-serif; font-size: 11px; margin-bottom: 0; margin-right: 1px; background-color: rgb(255, 255, 255); margin-top: 0; text-transform: none; font-style: normal; margin-left: 1px; word-spacing: 0px; text-align: justify" class="paper_abstract">
+      TDD basiert auf diesem Muster:
+    </p>
+    <ul style="font-weight: normal; padding-left: 1pt; list-style-position: outside; letter-spacing: normal; text-indent: 0px; line-height: normal; white-space: normal; font-variant: normal; color: rgb(0, 0, 0); font-family: arial, helvetica, sans-serif; font-size: 11px; margin-bottom: 0; list-style-type: disc; background-color: rgb(255, 255, 255); margin-top: 0pt; text-transform: none; font-style: normal; margin-left: 0; word-spacing: 0px; text-align: justify" class="paper_abstract_list">
+      <li style="margin-top: 1pt; text-indent: 0; margin-bottom: 1pt; padding-left: 1pt; margin-left: 0">
+        Schreibe einen Test
+      </li>
+      <li style="margin-top: 1pt; text-indent: 0; margin-bottom: 1pt; padding-left: 1pt; margin-left: 0">
+        Schreibe exakt soviel Quellcode, da&#223; alle Tests durchlaufen (inkl. dem Neuen)
+      </li>
+      <li style="margin-top: 1pt; text-indent: 0; margin-bottom: 1pt; padding-left: 1pt; margin-left: 0">
+        R&#228;ume den Quellcode auf (ohne die Tests zu verletzen)
+      </li>
+    </ul>
+    <p style="font-weight: normal; letter-spacing: normal; text-indent: 0px; line-height: normal; white-space: normal; font-variant: normal; color: rgb(0, 0, 0); font-family: arial, helvetica, sans-serif; font-size: 11px; margin-bottom: 0; margin-right: 1px; background-color: rgb(255, 255, 255); margin-top: 0; text-transform: none; font-style: normal; margin-left: 1px; word-spacing: 0px; text-align: justify" class="paper_abstract">
+      Was sich in der Theorie so einfach anh&#246;rt ist es in der Praxis leider nicht immer. In diesem Workshop sollen daher die wichtigsten Grundlagen vermittelt werden, um selbst erfolgreich testgetrieben zu arbeiten.
+    </p>
+    <p style="font-weight: normal; letter-spacing: normal; text-indent: 0px; line-height: normal; white-space: normal; font-variant: normal; color: rgb(0, 0, 0); font-family: arial, helvetica, sans-serif; font-size: 11px; margin-bottom: 0; margin-right: 1px; background-color: rgb(255, 255, 255); margin-top: 0; text-transform: none; font-style: normal; margin-left: 1px; word-spacing: 0px; text-align: justify" class="paper_abstract">
+      Dazu geh&#246;ren die u.a. die folgenden Fragen:
+    </p>
+    <ul style="font-weight: normal; padding-left: 1pt; list-style-position: outside; letter-spacing: normal; text-indent: 0px; line-height: normal; white-space: normal; font-variant: normal; color: rgb(0, 0, 0); font-family: arial, helvetica, sans-serif; font-size: 11px; margin-bottom: 0; list-style-type: disc; background-color: rgb(255, 255, 255); margin-top: 0pt; text-transform: none; font-style: normal; margin-left: 0; word-spacing: 0px; text-align: justify" class="paper_abstract_list">
+      <li style="margin-top: 1pt; text-indent: 0; margin-bottom: 1pt; padding-left: 1pt; margin-left: 0">
+        Wie sieht ein guter Test eigentlich aus? Was sollte dieser abdecken?
+      </li>
+      <li style="margin-top: 1pt; text-indent: 0; margin-bottom: 1pt; padding-left: 1pt; margin-left: 0">
+        Auf fachlicher Ebene: WAS soll getestet werden
+      </li>
+      <li style="margin-top: 1pt; text-indent: 0; margin-bottom: 1pt; padding-left: 1pt; margin-left: 0">
+        Auf technischer Ebene: WIE schreibt man einen sauberen UnitTest?
+      </li>
+      <li style="margin-top: 1pt; text-indent: 0; margin-bottom: 1pt; padding-left: 1pt; margin-left: 0">
+        Welche &#8220;Test Smells&#8221; gibt es und wie schaffe ich es mich davon zu l&#246;sen?
+      </li>
+      <li style="margin-top: 1pt; text-indent: 0; margin-bottom: 1pt; padding-left: 1pt; margin-left: 0">
+        Was &#228;ndert sich, wenn man nicht nur einige wenige Tests hat, sondern viele bis sehr viele (evtl. gar mehrere Tausend)?
+      </li>
+      <li style="margin-top: 1pt; text-indent: 0; margin-bottom: 1pt; padding-left: 1pt; margin-left: 0">
+        Wo sind hier die Herausforderungen?
+      </li>
+      <li style="margin-top: 1pt; text-indent: 0; margin-bottom: 1pt; padding-left: 1pt; margin-left: 0">
+        Kann man evtl. auch Dinge vereinfachen (z.B. durch Einsatz von Factories)?
+      </li>
+      <li style="margin-top: 1pt; text-indent: 0; margin-bottom: 1pt; padding-left: 1pt; margin-left: 0">
+        Welche M&#246;glichkeiten gibt es langsam ablaufende Testszenarien zu beschleunigen
+      </li>
+      <li style="margin-top: 1pt; text-indent: 0; margin-bottom: 1pt; padding-left: 1pt; margin-left: 0">
+        Wie kann man die architektonischen Voraussetzungen daf&#252;r schaffen?
+      </li>
+    </ul>
+    <p style="font-weight: normal; letter-spacing: normal; text-indent: 0px; line-height: normal; white-space: normal; font-variant: normal; color: rgb(0, 0, 0); font-family: arial, helvetica, sans-serif; font-size: 11px; margin-bottom: 0; margin-right: 1px; background-color: rgb(255, 255, 255); margin-top: 0; text-transform: none; font-style: normal; margin-left: 1px; word-spacing: 0px; text-align: justify" class="paper_abstract">
+      All diese Fragen, und hoffentlich noch einige mehr, soll dieser Workshop beantworten.
+    </p>
+    <p style="font-weight: normal; letter-spacing: normal; text-indent: 0px; line-height: normal; white-space: normal; font-variant: normal; color: rgb(0, 0, 0); font-family: arial, helvetica, sans-serif; font-size: 11px; margin-bottom: 0; margin-right: 1px; background-color: rgb(255, 255, 255); margin-top: 0; text-transform: none; font-style: normal; margin-left: 1px; word-spacing: 0px; text-align: justify" class="paper_abstract">
+      Gedacht ist, nicht nur eine theoretische Einf&#252;hrung zu geben, sondern die Teilnehmer das an ihren mitgebrachten Notebooks anhand eines Beispielprojekts gleich direkt ausprobieren zu lassen. Dazu wird es nat&#252;rlich ein vorgefertigtes Setup geben, sodass wir m&#246;glichst ohne Zeitverlust starten k&#246;nnen.
+    </p>
+    <p style="font-weight: normal; letter-spacing: normal; text-indent: 0px; line-height: normal; white-space: normal; font-variant: normal; color: rgb(0, 0, 0); font-family: arial, helvetica, sans-serif; font-size: 11px; margin-bottom: 0; margin-right: 1px; background-color: rgb(255, 255, 255); margin-top: 0; text-transform: none; font-style: normal; margin-left: 1px; word-spacing: 0px; text-align: justify" class="paper_abstract">
+      Deswegen Achtung: Hier soll richtig gecodet werden :).
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1351939587034" ID="ID_372418641" MODIFIED="1351942454946" POSITION="right" TEXT="TDD">
+<richcontent TYPE="NOTE"><html>   <head>        </head>   <body>     <ol>       <li>         Schreibe einen Test       </li>       <li>         Schreibe genau so viel Quellcode, da&#223; der Test durchl&#228;uft       </li>       <li>         R&#228;ume auf       </li>     </ol>     <p>       Anwendungsgebiete:     </p>     <p>            </p>     <ul>       <li>         Extreme Programming          <ul>           <li>             Pair Programming           </li>         </ul>       </li>     </ul>     <p>            </p>   </body> </html></richcontent>
+<node CREATED="1351941768327" ID="ID_1119997191" MODIFIED="1351942654367" TEXT="Warum eigentlich TDD und nicht danach"/>
+<node CREATED="1351942455456" ID="ID_1026392424" MODIFIED="1351942592900" TEXT="Vorgehensweise">
+<node CREATED="1351942477118" ID="ID_968167651" MODIFIED="1351949783156" TEXT="Schreibe einen Test der fehlschl&#xe4;gt (Red)">
+<icon BUILTIN="stop"/>
+</node>
+<node CREATED="1351942498597" ID="ID_1217732300" MODIFIED="1351949787640" TEXT="Bringe den Test mit m&#xf6;glichst wenig Aufwand zum Laufen (Green)">
+<icon BUILTIN="go"/>
+</node>
+<node CREATED="1351942518604" ID="ID_599420916" MODIFIED="1351949792223" TEXT="Verbessere den Code (Refactor)">
+<icon BUILTIN="go"/>
+</node>
+</node>
+<node CREATED="1351941383094" ID="ID_1903570043" MODIFIED="1351941957877" TEXT="Clean Tests">
+<node CREATED="1351941439503" ID="ID_147443777" MODIFIED="1351942129618" TEXT="Build - Operate - Test">
+<richcontent TYPE="NOTE"><html>   <head>        </head>   <body>     <ul>       <li>         Erst initialisieren aller notwendigen Objekte       </li>       <li>         Dann aufrufen der zu testenden Methode       </li>       <li>         Dann pr&#252;fen der gelieferten Ergebnisse mittels &quot;Assert&quot;       </li>     </ul>   </body> </html></richcontent>
+</node>
+<node CREATED="1351941508300" ID="ID_745902391" MODIFIED="1351942321916" TEXT="Auch bei Unittests muss auf sauberen Code geachtet werden">
+<richcontent TYPE="NOTE"><html>   <head>        </head>   <body>     <ul>       <li>         Sogar eher noch mehr als beim &quot;eigentlichen&quot; Code!       </li>       <li>         Tests m&#252;ssen w&#228;hrend der Entwicklung oft ge&#228;ndert, angepasst und erweitert werden. Unsauberer Code erschwert dies unn&#246;tig!       </li>     </ul>   </body> </html></richcontent>
+</node>
+</node>
+<node CREATED="1351941386962" ID="ID_793812816" MODIFIED="1351941403708" TEXT="One Assert per Test"/>
+<node CREATED="1351941416824" ID="ID_1244373996" MODIFIED="1351941419856" TEXT="F.I.R.S.T.">
+<node CREATED="1351941536355" ID="ID_1326674018" MODIFIED="1351941539192" TEXT="Fast"/>
+<node CREATED="1351941539642" ID="ID_1775007824" MODIFIED="1351941542967" TEXT="Independent"/>
+<node CREATED="1351941543770" ID="ID_1575221981" MODIFIED="1351941549510" TEXT="Repeatable"/>
+<node CREATED="1351941567161" ID="ID_393917059" MODIFIED="1351941585309" TEXT="Self Validating"/>
+<node CREATED="1351941586039" ID="ID_775691140" MODIFIED="1351941587997" TEXT="Timely"/>
+</node>
+<node CREATED="1351941453456" ID="ID_923630888" MODIFIED="1351941459935" TEXT="Keep tests short"/>
+<node CREATED="1351941525089" ID="ID_995943279" MODIFIED="1351941535782" TEXT="Grenzf&#xe4;lle betrachten"/>
+<node CREATED="1351941600481" ID="ID_863905010" MODIFIED="1351941758177" TEXT="Eigene Fehlermeldungen nutzen">
+<richcontent TYPE="NOTE"><html>   <head>        </head>   <body>     <p>       Die M&#246;glichkeiten moderner Testframeworks ausnutzen und den Asserts eigene Fehlermeldungen mitgeben     </p>   </body> </html></richcontent>
+</node>
+</node>
+<node CREATED="1351939943165" ID="ID_1505609094" MODIFIED="1351939946259" POSITION="left" TEXT="TestSmells"/>
+<node CREATED="1351940004322" ID="ID_283330446" MODIFIED="1351940008710" POSITION="left" TEXT="Testfactories"/>
+<node CREATED="1351940039792" ID="ID_128727764" MODIFIED="1351940054460" POSITION="left" TEXT="Architekturvoraussetzungen">
+<node CREATED="1351960204008" ID="ID_174175255" MODIFIED="1351960227976" TEXT="Dependency Injection"/>
+<node CREATED="1351960350174" ID="ID_1229318459" MODIFIED="1351960364891" TEXT="Testspezifische Kindklassen"/>
+</node>
+<node CREATED="1351941140232" ID="ID_1170523317" MODIFIED="1351941161569" POSITION="right" TEXT="Welche Arten von automatisierten Tests gibt es eigentlich?"/>
+<node CREATED="1351942896458" ID="ID_1067200972" MODIFIED="1351960712329" POSITION="right" TEXT="Schwierigkeiten und Fallstricke bei Tests">
+<node CREATED="1351940010265" ID="ID_1227188988" MODIFIED="1351960763974" TEXT="Wie beschleunige ich langsam ablaufende Tests">
+<node CREATED="1351960218149" ID="ID_1683174791" MODIFIED="1351960803049" TEXT="Konzentration aufs Wesentliche">
+<node CREATED="1351960291127" ID="ID_1916184326" MODIFIED="1351960319003" TEXT="Units richtig bemessen">
+<node CREATED="1351960435551" ID="ID_869925015" MODIFIED="1351960458312" TEXT="So klein wie irgend m&#xf6;glich"/>
+<node CREATED="1351960472207" ID="ID_1593836663" MODIFIED="1351960493376" TEXT="Testdauer im Millisekundenbereich"/>
+</node>
+<node CREATED="1351960379847" ID="ID_501826312" MODIFIED="1351960812846" TEXT="Einsatz von Mocking Frameworks, Stubs, Dummys etc.">
+<node CREATED="1351960405576" ID="ID_706904320" MODIFIED="1351960431006" TEXT="Easymock"/>
+<node CREATED="1351960419547" ID="ID_137696041" MODIFIED="1351960424566" TEXT="Mockito"/>
+</node>
+<node CREATED="1351960227970" ID="ID_428911974" MODIFIED="1351960234108" TEXT="Keine DB-Tests in der BL">
+<node CREATED="1351960582651" ID="ID_1448935384" MODIFIED="1351960595649" TEXT="Alternative Persistenzen">
+<node CREATED="1351960597171" ID="ID_734304241" MODIFIED="1351960609512" TEXT="In Memory DB"/>
+<node CREATED="1351960615553" ID="ID_89703059" MODIFIED="1351960713828" TEXT=""/>
+</node>
+</node>
+<node CREATED="1351960237420" ID="ID_1085920512" MODIFIED="1351960280206" TEXT="DB-Tests z.B. zuerst mit In Memory DB durchspielen"/>
+<node CREATED="1351960819173" ID="ID_1156260737" MODIFIED="1351960820639" TEXT="test"/>
+</node>
+</node>
+<node CREATED="1351939958108" ID="ID_439776599" MODIFIED="1351939977384" TEXT="Was passiert bei vielen Tests">
+<node CREATED="1351960705923" ID="ID_1748656890" MODIFIED="1351960720372" TEXT="Aggregation zu Testsuiten"/>
+</node>
+<node CREATED="1351943000010" ID="ID_1529019351" MODIFIED="1351943026250" TEXT="Tests mit nebenl&#xe4;ufigen Prozessen"/>
+<node CREATED="1351943028092" ID="ID_1340431742" MODIFIED="1351943063071" TEXT="Tests bei GUI Code">
+<node CREATED="1351943070546" ID="ID_51905672" MODIFIED="1351943077759" TEXT="Don&apos;t do it ;-)"/>
+<node CREATED="1351943078458" ID="ID_414304436" MODIFIED="1351943090190" TEXT="Unit Tests f&#xfc;r BL"/>
+<node CREATED="1351943091180" ID="ID_669042454" MODIFIED="1351943121844" TEXT="Patterns einsetzen um GUI Code schlank zu halten">
+<node CREATED="1351943122703" ID="ID_780099471" MODIFIED="1351943143819" TEXT="MVC/MVP"/>
+</node>
+<node CREATED="1351943209812" ID="ID_382031570" MODIFIED="1351943222192" TEXT="Moderne Frameworks nutzen"/>
+</node>
+</node>
+<node CREATED="1351949737133" ID="ID_1124800040" MODIFIED="1351949808714" POSITION="right" TEXT="Praktischer Teil">
+<icon BUILTIN="penguin"/>
+<node CREATED="1351949833205" ID="ID_422806447" MODIFIED="1351950051884" TEXT="">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <h2>
+      Mars Rover Kata
+    </h2>
+    <div class="entry">
+      <p>
+        Develop an api that moves a rover around on a grid.
+      </p>
+      <p>
+        You are given the initial starting point (x,y) of a rover and the direction (N,S,E,W) it is facing.
+      </p>
+      <p>
+        The rover receives a character array of commands.
+      </p>
+      <p>
+        Implement commands that move the rover forward/backward (f,b).
+      </p>
+      <p>
+        Implement commands that turn the rover left/right (l,r).
+      </p>
+      <p>
+        Implement wrapping from one edge of the grid to another. (planets are spheres after all)
+      </p>
+      <p>
+        Implement obstacle detection before each move to a new square. If a given sequence of commands encounters an obstacle, the rover moves up to the last possible point and reports the obstacle.
+      </p>
+    </div>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</map>
---
+++ XP Days 2012.mm
@@ -0,0 +1,6 @@
+<map version="1.0.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1351937145341" ID="ID_682547659" MODIFIED="1351939556536" TEXT="XP Days 2012">
+<node CREATED="1351939372357" ID="ID_231520922" MODIFIED="1351939375279" POSITION="right" TEXT="Test"/>
+</node>
+</map>
